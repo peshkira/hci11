6d2126a2fac59ce38015e3b0ee5e6b574d74c47d
Petar Petrov <me@petarpetrov.org>
2011-04-17 20:16:33 +0200

    first functional prototype

--- questo/src/com/questo/android/QuestoHome.java
+++ questo/src/com/questo/android/QuestoHome.java
@@ -7,17 +7,15 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 
-import com.questo.android.view.TopBar;
-
 public class QuestoHome extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        TopBar topbar = (TopBar)findViewById(R.id.topbar);
-        topbar.setTopBarLabel("LolCat!");
-        topbar.addButtonLeftMost(getApplicationContext(), "+");
+//        TopBar topbar = (TopBar)findViewById(R.id.topbar);
+//        topbar.setTopBarLabel("LolCat!");
+//        topbar.addButtonLeftMost(getApplicationContext(), "+");
 
         this.initViews();
 
@@ -26,35 +24,40 @@ public class QuestoHome extends Activity {
     private void initViews() {
         int id = R.id.imgQuests;
         ImageView v = (ImageView) findViewById(R.id.imgQuests);
-        v.setOnTouchListener(new MenuOnTouchListener());
+        v.setOnTouchListener(new MenuOnTouchListener("quests"));
 
         id = R.id.imgTournaments;
         v = (ImageView) findViewById(id);
-        v.setOnTouchListener(new MenuOnTouchListener());
+        v.setOnTouchListener(new MenuOnTouchListener("tournaments"));
 
         id = R.id.imgProfile;
         v = (ImageView) findViewById(id);
-        v.setOnTouchListener(new MenuOnTouchListener());
+        v.setOnTouchListener(new MenuOnTouchListener("profile"));
 
         id = R.id.imgCompanions;
         v = (ImageView) findViewById(id);
-        v.setOnTouchListener(new MenuOnTouchListener());
+        v.setOnTouchListener(new MenuOnTouchListener("companions"));
 
         id = R.id.imgTrophies;
         v = (ImageView) findViewById(id);
-        v.setOnTouchListener(new MenuOnTouchListener());
+        v.setOnTouchListener(new MenuOnTouchListener("trophies"));
 
         id = R.id.imgSettings;
         v = (ImageView) findViewById(id);
-        v.setOnTouchListener(new MenuOnTouchListener());
+        v.setOnTouchListener(new MenuOnTouchListener("settings"));
     }
 
     private void navigate(String to) {
-        // TODO get the view Name
-        // and start a new intent to this view
+        System.out.println("NAVIGATE!!! " + to);
     }
 
     private class MenuOnTouchListener implements OnTouchListener {
+        
+        private String comp;
+
+        public MenuOnTouchListener(String comp) {
+            this.comp = comp;
+        }
 
         @Override
         public boolean onTouch(View v, MotionEvent evt) {
@@ -63,12 +66,11 @@ public class QuestoHome extends Activity {
             switch (evt.getAction()) {
             case MotionEvent.ACTION_DOWN: {
                 view.setImageResource(R.drawable.flashget);
-                // QuestoHome.this.navigate();
                 break;
             }
             case MotionEvent.ACTION_UP: {
                 view.setImageResource(R.drawable.games);
-                // QuestoHome.this.navigate();
+                QuestoHome.this.navigate(comp);
                 break;
             }
             }
