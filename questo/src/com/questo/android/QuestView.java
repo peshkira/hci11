b58ee39bc31fe3a28a03852049d0a4e5c0136e38
Rudolf Mildner <r.mildner@gmx.at>
2011-04-20 12:29:05 +0200

    1:adding internet permission and map library to the manifest 2:bringing the map view to work

--- questo/src/com/questo/android/QuestView.java
+++ questo/src/com/questo/android/QuestView.java
@@ -1,14 +1,32 @@
 package com.questo.android;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class QuestView extends Activity{
+import com.google.android.maps.MapActivity;
+import com.questo.android.view.TopBar;
+
+public class QuestView extends MapActivity{
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		initView();
+	}
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+	
+	
+	private void initView(){
 		setContentView(R.layout.quests);
+		
+		TopBar topBar = (TopBar)findViewById(R.id.topbar);
+		topBar.addButtonLeftMost(this, "-");
+		topBar.addButtonLeftMost(this, "+");
+		
+		
 	}
 }
