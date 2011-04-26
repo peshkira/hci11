48515d833515e42b8981305085aecde09976c274
Petar Petrov <me@petarpetrov.org>
2011-04-26 14:50:04 +0300

    fixing home screen with a grid view

--- questo/src/com/questo/android/QuestoHome.java
+++ questo/src/com/questo/android/QuestoHome.java
@@ -13,62 +13,49 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.questo.android.common.Constants;
 import com.questo.android.helper.UUIDgen;
 import com.questo.android.model.Notification;
 import com.questo.android.model.Notification.Type;
 
 public class QuestoHome extends Activity {
 
+    private GridView mainGrid;
+
+    private static final String[] menus = { "Quests", "Tournaments", "Profile", "Companions", "Trophy Room", "Settings" };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        // TopBar topbar = (TopBar)findViewById(R.id.topbar);
-        // topbar.setTopBarLabel("LolCat!");
-        // topbar.addButtonLeftMost(getApplicationContext(), "+");
 
         this.initViews();
     }
 
     private void initViews() {
-        int id = R.id.imgQuests;
-        ImageView v = (ImageView) findViewById(R.id.imgQuests);
-        v.setOnClickListener(new MenuOnTouchListener("quests"));
 
-        id = R.id.imgTournaments;
-        v = (ImageView) findViewById(id);
-        v.setOnClickListener(new MenuOnTouchListener("tournaments"));
-
-        id = R.id.imgProfile;
-        v = (ImageView) findViewById(id);
-        v.setOnClickListener(new MenuOnTouchListener("profile"));
-
-        id = R.id.imgCompanions;
-        v = (ImageView) findViewById(id);
-        v.setOnClickListener(new MenuOnTouchListener("companions"));
-
-        id = R.id.imgTrophies;
-        v = (ImageView) findViewById(id);
-        v.setOnClickListener(new MenuOnTouchListener("trophies"));
-
-        id = R.id.imgSettings;
-        v = (ImageView) findViewById(id);
-        v.setOnClickListener(new MenuOnTouchListener("settings"));
-
+        this.mainGrid = (GridView) findViewById(R.id.homeGrid);
+        this.mainGrid.setAdapter(new ImageAdapter());
+        
         ListView watchtower = (ListView) findViewById(R.id.watchtower);
         //
         QuestoListAdapter adapt = new QuestoListAdapter();
-        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST, "<b>Cato</b> has completed quest <b>Chillhouse of Terror</b>.", null, null, new Date()));
-        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST, "<b>Nuno</b> completed a quest.", null, null, new Date()));
-        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST, "<b>Lolcat</b> completed a quest.", null, null, new Date()));
-        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST, "<b>Aragorn</b> completed a quest.", null, null, new Date()));
-        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST, "<b>Cato</b> completed a quest.", null, null, new Date()));
-        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST, "<b>Gandalf</b> completed a quest.", null, null, new Date()));
+        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST,
+                "<b>Cato</b> has completed quest <b>Chillhouse of Terror</b>.", null, null, new Date()));
+        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST, "<b>Nuno</b> completed a quest.",
+                null, null, new Date()));
+        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST,
+                "<b>Lolcat</b> completed a quest.", null, null, new Date()));
+        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST,
+                "<b>Aragorn</b> completed a quest.", null, null, new Date()));
+        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST, "<b>Cato</b> completed a quest.",
+                null, null, new Date()));
+        adapt.addItem(new Notification(UUIDgen.getUUID(), Type.USER_COMPLETED_QUEST,
+                "<b>Gandalf</b> completed a quest.", null, null, new Date()));
         watchtower.setAdapter(adapt);
 
     }
@@ -76,15 +63,29 @@ public class QuestoHome extends Activity {
     private void navigate(String to) {
         System.out.println("NAVIGATE!!! " + to);
         Intent navTo = null;
-        if (to.equals("profile")) {
-            navTo = new Intent(this, UserProfile.class);
-        }
-        if (to.equals("quests")) {
-        	navTo = new Intent(this, QuestMapView.class);
+        if (to.equals(menus[0])) {
+            navTo = new Intent(this, QuestMapView.class);
         }
         
-        if(navTo != null){
-        	startActivity(navTo);
+        if (to.equals(menus[1])) {
+
+        }
+        
+        if (to.equals(menus[2])) {
+            navTo = new Intent(this, UserProfile.class);
+        }
+        
+        if (to.equals(menus[3])) {
+        }
+
+        if (to.equals(menus[4])) {
+        }
+        
+        if (to.equals(menus[5])) {
+        }
+        
+        if (navTo != null) {
+            startActivity(navTo);
         }
     }
 
@@ -153,5 +154,70 @@ public class QuestoHome extends Activity {
 
     public static class ViewHolder {
         public TextView textView;
+    }
+
+    private class ImageAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return menus.length;
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v;
+
+            if (convertView == null) {
+                LayoutInflater li = getLayoutInflater();
+                v = li.inflate(R.layout.homeicon, null);
+                TextView tv = (TextView) v.findViewById(R.id.home_icon_text);
+                tv.setText(menus[position]);
+                ImageView iv = (ImageView) v.findViewById(R.id.home_icon_image);
+                iv.setImageResource(this.getImage(position));
+                iv.setOnClickListener(new MenuOnTouchListener(menus[position]));
+            } else {
+                v = convertView;
+            }
+
+            return v;
+        }
+
+        private int getImage(int pos) {
+            int id = -1;
+            switch (pos) {
+            case 0:
+                id = R.drawable.quests;
+                break;
+            case 1:
+                id = R.drawable.tournaments;
+                break;
+            case 2:
+                id = R.drawable.profiles;
+                break;
+            case 3:
+                id = R.drawable.companions;
+                break;
+            case 4:
+                id = R.drawable.trophies;
+                break;
+            case 5:
+                id = R.drawable.settings;
+                break;
+            default:
+                id = R.drawable.arrow_target;
+                break;
+            }
+
+            return id;
+        }
     }
 }
