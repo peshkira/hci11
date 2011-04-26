05d3c346f40309b4c637db16aaca0edc2dce3d35
Petar Petrov <me@petarpetrov.org>
2011-04-26 13:05:21 +0300

    placedetails is ready

--- questo/src/com/questo/android/common/Constants.java
+++ questo/src/com/questo/android/common/Constants.java
@@ -6,9 +6,9 @@ public final class Constants {
     public static final String TOPBAR_LABEL = "app.questo.topbar.label";
     public static final String NR_QUESTIONS = "app.questo.questions.number";
     public static final String NR_ANSWERED_QUESTIONS = "app.questo.questions.number.answered";
-    public static final String NR_QUESTIONS_LABEL = "This place has " + Constants.PLACEHOLDER + " questions.";
-    public static final String NR_ANSWERED_QUESTIONS_LABEL = "You have already answered " + Constants.PLACEHOLDER
-            + " questions correctly here.";
+    public static final String NR_QUESTIONS_LABEL = "This place has <b>" + Constants.PLACEHOLDER + " questions.</b>";
+    public static final String NR_ANSWERED_QUESTIONS_LABEL = "You have already answered <b>" + Constants.PLACEHOLDER
+            + " questions</b> correctly here.";
 
     private Constants() {
 
