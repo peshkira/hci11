21c16e40ef64d7cd0f1c1fe682c461dd3a5bc318
Petar Petrov <me@petarpetrov.org>
2011-04-26 16:52:07 +0300

    adding progressbar

--- questo/src/com/questo/android/QuestionView.java
+++ questo/src/com/questo/android/QuestionView.java
@@ -1,9 +1,11 @@
 package com.questo.android;
 
-import com.questo.android.view.TopBar;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.questo.android.common.Constants;
+import com.questo.android.view.TopBar;
 
 public class QuestionView extends Activity {
 
@@ -18,7 +20,17 @@ public class QuestionView extends Activity {
     }
 
     private void init(Bundle extras) {
+        int q = extras.getInt(Constants.QUESTIONS);
+        int count = extras.getInt(Constants.NR_QUESTIONS);
+        
         this.topbar = (TopBar) findViewById(R.id.topbar);
-        //this.topbar.setTopBarLabel(label) 
+        this.topbar.setTopBarLabel(Constants.QUESTION_PROGRESS.replaceFirst("\\{\\}", q+"").replace("{}", count+"")); 
+        ProgressBar bar = (ProgressBar) findViewById(R.id.progressbar);
+        bar.setProgressDrawable(getResources().getDrawable(R.drawable.green_progress));
+        bar.setProgress(this.calcProgress(q, count));
+    }
+
+    private int calcProgress(int q, int count) {
+        return (int) ((q * 100)/count);
     }
 }
--- questo/src/com/questo/android/common/Constants.java
+++ questo/src/com/questo/android/common/Constants.java
@@ -9,6 +9,8 @@ public final class Constants {
     public static final String NR_QUESTIONS_LABEL = "This place has <b>" + Constants.PLACEHOLDER + " questions.</b>";
     public static final String NR_ANSWERED_QUESTIONS_LABEL = "You have already answered <b>" + Constants.PLACEHOLDER
             + " questions</b> correctly here.";
+    public static final String QUESTION_PROGRESS = "Question {} of {}";
+    public static final String QUESTIONS = "app.questo.questions.question";
 
     private Constants() {
 
