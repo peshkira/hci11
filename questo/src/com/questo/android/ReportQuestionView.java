498936db41ff2bb7b366bc6404f6748ae0510880
Petar Petrov <me@petarpetrov.org>
2011-05-08 14:37:51 +0200

    report screen prototype

---
+++ questo/src/com/questo/android/ReportQuestionView.java
@@ -0,0 +1,35 @@
+package com.questo.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class ReportQuestionView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.question_report);
+        this.init(this.getIntent().getExtras());
+    }
+
+    private void init(Bundle extras) {
+        TextView question = (TextView) findViewById(R.id.txt_question);
+        question.setText(Html.fromHtml("<big><b>This is a dummy question?</b></big>"));
+
+        EditText area = (EditText) findViewById(R.id.txt_area_report);
+
+        Button submit = (Button) findViewById(R.id.btn_report);
+        
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_reason);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.report_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
