90a4da933de4b3771dab8e625bbc8c1847f48a91
Manuel Maly <manuel.maly@gmail.com>
2011-05-08 14:24:01 +0200

    reverting questionview, readding changes

--- questo/src/com/questo/android/QuestionView.java
+++ questo/src/com/questo/android/QuestionView.java
@@ -34,113 +34,113 @@ import com.questo.android.view.TopBar;
 
 public class QuestionView extends Activity {
 
-	private TopBar topbar;
+    private TopBar topbar;
 
-	private ModelManager mngr;
+    private ModelManager mngr;
 
-	private RadioGroup rg;
+    private RadioGroup rg;
 
-	private int currentQuestion;
+    private int currentQuestion;
 
-	private String questUuid;
+    private String questUuid;
 
-	private Place place;
+    private Place place;
 
-	private EditText input;
+    private EditText input;
+    
+    private QuestionTimer timer;
 
-	private QuestionTimer timer;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question);
+        this.init(this.getIntent().getExtras());
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.question);
-		this.init(this.getIntent().getExtras());
+    }
 
-	}
+    private void init(Bundle extras) {
+        mngr = ((App) getApplicationContext()).getModelManager();
 
-	private void init(Bundle extras) {
-		mngr = ((App) getApplicationContext()).getModelManager();
+        questUuid = extras.getString(Constants.TRANSITION_OBJECT_UUID);
 
-		questUuid = extras.getString(Constants.TRANSITION_OBJECT_UUID);
+        Quest quest = (Quest) mngr.getGenericObjectByUuid(questUuid, Quest.class);
 
-		Quest quest = (Quest) mngr.getGenericObjectByUuid(questUuid, Quest.class);
+        place = quest.getPlace();
+        mngr.refresh(place, Place.class);
 
-		place = quest.getPlace();
-		mngr.refresh(place, Place.class);
+        int size = place.getQuestions().size();
+        currentQuestion = extras.getInt(Constants.NR_ANSWERED_QUESTIONS);
+        int count = 10;
+        if (size < 10) {
+            count = size;
+        }
 
-		int size = place.getQuestions().size();
-		currentQuestion = extras.getInt(Constants.NR_ANSWERED_QUESTIONS);
-		int count = 10;
-		if (size < 10) {
-			count = size;
-		}
+        Question qtn = (Question) place.getQuestions().toArray()[currentQuestion];
+        String type = qtn.getType().name();
 
-		Question qtn = (Question) place.getQuestions().toArray()[currentQuestion];
-		String type = qtn.getType().name();
+        this.topbar = (TopBar) findViewById(R.id.topbar);
+        this.topbar.setTopBarLabel(Constants.QUESTION_PROGRESS.replaceFirst("\\{\\}", currentQuestion + 1 + "")
+                .replace("{}", count + ""));
 
-		this.topbar = (TopBar) findViewById(R.id.topbar);
-		this.topbar.setTopBarLabel(Constants.QUESTION_PROGRESS.replaceFirst("\\{\\}", currentQuestion + 1 + "")
-				.replace("{}", count + ""));
+        ProgressBar bar = (ProgressBar) findViewById(R.id.progressbar);
+        bar.setProgressDrawable(getResources().getDrawable(R.drawable.bg_progressbar));
+        bar.setProgress(this.calcProgress(currentQuestion + 1, count));
 
-		ProgressBar bar = (ProgressBar) findViewById(R.id.progressbar);
-		bar.setProgressDrawable(getResources().getDrawable(R.drawable.bg_progressbar));
-		bar.setProgress(this.calcProgress(currentQuestion + 1, count));
+        final Button counter = this.topbar.addButtonLeftMost(getApplicationContext(), "30");
+        counter.setClickable(false);
 
-		final Button counter = this.topbar.addButtonLeftMost(getApplicationContext(), "30");
-		counter.setClickable(false);
+        TextView question = (TextView) findViewById(R.id.question);
+        question.setText(qtn.getQuestion());
 
-		TextView question = (TextView) findViewById(R.id.question);
-		question.setText(qtn.getQuestion());
+        LinearLayout layout = (LinearLayout) findViewById(R.id.qtn_questioncontent);
 
-		LinearLayout layout = (LinearLayout) findViewById(R.id.qtn_questioncontent);
+        if (type.equals(Question.Type.MULTIPLE_CHOICE.name())) {
+            PossibleAnswers answers = qtn.getPossibleAnswers();
+            this.createMultipleChoiceView(layout, answers.getAll());
 
-		if (type.equals(Question.Type.MULTIPLE_CHOICE.name())) {
-			PossibleAnswers answers = qtn.getPossibleAnswers();
-			this.createMultipleChoiceView(layout, answers.getAll());
+        } else if (type.equals(Question.Type.PLAINTEXT.name())) {
+            this.createPlainTextView(layout);
 
-		} else if (type.equals(Question.Type.PLAINTEXT.name())) {
-			this.createPlainTextView(layout);
+        } else if (type.equals(Question.Type.NUMBERS_GUESSING.name())) {
+            this.createNumberGuessView(layout);
+        }
 
-		} else if (type.equals(Question.Type.NUMBERS_GUESSING.name())) {
-			this.createNumberGuessView(layout);
-		}
+        LayoutInflater li = getLayoutInflater();
 
-		LayoutInflater li = getLayoutInflater();
+        LinearLayout buttonsLayout = (LinearLayout)findViewById(R.id.qtn_answerbuttons);
+        View buttons = li.inflate(R.layout.answer_buttons, null);
+        buttonsLayout.addView(buttons);
 
-		LinearLayout buttonsLayout = (LinearLayout) findViewById(R.id.qtn_answerbuttons);
-		View buttons = li.inflate(R.layout.answer_buttons, null);
-		buttonsLayout.addView(buttons);
+        Button btnAnswer = (Button) findViewById(R.id.btn_answer);
+        btnAnswer.setOnClickListener(new AnswerClickListener(qtn));
+        Button btnNoIdea = (Button) findViewById(R.id.btn_noidea);
+        btnNoIdea.setOnClickListener(new NoIdeaClickListener(qtn));
 
-		Button btnAnswer = (Button) findViewById(R.id.btn_answer);
-		btnAnswer.setOnClickListener(new AnswerClickListener(qtn));
-		Button btnNoIdea = (Button) findViewById(R.id.btn_noidea);
-		btnNoIdea.setOnClickListener(new NoIdeaClickListener(qtn));
+        this.timer = new QuestionTimer(30000, 1000, counter, qtn.getCorrectAnswer().get().getAnswer());
+        this.timer.start();
+        
+    }
 
-		this.timer = new QuestionTimer(30000, 1000, counter, qtn.getCorrectAnswer().get().getAnswer());
-		this.timer.start();
+    private void createNumberGuessView(LinearLayout layout) {
+        LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+        input = new EditText(this);
+        input.setLayoutParams(params);
+        input.setSingleLine();
+        input.setHint("e.g. 1234");
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        layout.addView(input);
 
-	}
+    }
 
-	private void createNumberGuessView(LinearLayout layout) {
-		LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-		input = new EditText(this);
-		input.setLayoutParams(params);
-		input.setSingleLine();
-		input.setHint("e.g. 1234");
-		input.setInputType(InputType.TYPE_CLASS_NUMBER);
-		layout.addView(input);
+    private void createPlainTextView(LinearLayout layout) {
+        LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+        input = new EditText(this);
+        input.setLayoutParams(params);
+        input.setSingleLine();
+        input.setHint("your answer");
+        layout.addView(input);
 
-	}
-
-	private void createPlainTextView(LinearLayout layout) {
-		LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-		input = new EditText(this);
-		input.setLayoutParams(params);
-		input.setSingleLine();
-		input.setHint("your answer");
-		layout.addView(input);
-
-	}
+    }
 
 	private void createMultipleChoiceView(LinearLayout layout, PossibleAnswer[] answers) {
 		final RadioButton[] rb = new RadioButton[5];
@@ -161,146 +161,148 @@ public class QuestionView extends Activity {
 		layout.addView(rg);
 	}
 
-	private int calcProgress(int q, int count) {
-		return (int) ((q * 100) / count);
-	}
+    private int calcProgress(int q, int count) {
+        return (int) ((q * 100) / count);
+    }
 
-	private class AnswerClickListener implements OnClickListener {
+    private class AnswerClickListener implements OnClickListener {
 
-		private Question question;
+        private Question question;
 
-		public AnswerClickListener(Question q) {
-			this.question = q;
-		}
+        public AnswerClickListener(Question q) {
+            this.question = q;
+        }
 
-		@Override
-		public void onClick(View v) {
-			boolean correct = false;
-			boolean selected = true;
+        @Override
+        public void onClick(View v) {
+            boolean correct = false;
+            boolean selected = true;
 
-			PossibleAnswerImpl ca = (PossibleAnswerImpl) question.getCorrectAnswer().get();
-			if (this.question.getType().equals(Question.Type.MULTIPLE_CHOICE)) {
-				int sel = QuestionView.this.rg.getCheckedRadioButtonId();
+            PossibleAnswerImpl ca = (PossibleAnswerImpl) question.getCorrectAnswer().get();
+            if (this.question.getType().equals(Question.Type.MULTIPLE_CHOICE)) {
+                int sel = QuestionView.this.rg.getCheckedRadioButtonId();
 
-				if (sel == -1) {
-					Toast.makeText(QuestionView.this, "Please select an answer!", Toast.LENGTH_SHORT).show();
-					selected = false;
-				} else {
-					RadioButton btn = (RadioButton) findViewById(sel);
-					// contains because of the &nbsp; characters...
-					if (btn.getText().toString().contains(ca.getAnswer().toString())) {
-						correct = true;
-					}
-				}
+                if (sel == -1) {
+                    Toast.makeText(QuestionView.this, "Please select an answer!", Toast.LENGTH_SHORT).show();
+                    selected = false;
+                } else {
+                    RadioButton btn = (RadioButton) findViewById(sel);
+                    // contains because of the &nbsp; characters...
+                    if (btn.getText().toString().contains(ca.getAnswer().toString())) {
+                        correct = true;
+                    }
+                }
 
-			} else if (this.question.getType().equals(Question.Type.NUMBERS_GUESSING)) {
-				if (QuestionView.this.input.getText().toString().equals(ca.getAnswer().toString())) {
-					correct = true;
-				}
-				System.out.println(ca.getAnswer());
+            } else if (this.question.getType().equals(Question.Type.NUMBERS_GUESSING)) {
+                if (QuestionView.this.input.getText().toString().equals(ca.getAnswer().toString())) {
+                    correct = true;
+                }
+                System.out.println(ca.getAnswer());
 
-			} else if (this.question.getType().equals(Question.Type.PLAINTEXT)) {
-				if (QuestionView.this.input.getText().toString().toLowerCase().equals(ca.getAnswer().toLowerCase())) {
-					correct = true;
-				}
-			}
+            } else if (this.question.getType().equals(Question.Type.PLAINTEXT)) {
+                if (QuestionView.this.input.getText().toString().toLowerCase().equals(ca.getAnswer().toLowerCase())) {
+                    correct = true;
+                }
+            }
 
-			if (selected) {
-				Intent intent = new Intent(QuestionView.this, QuestionResult.class);
-				intent.putExtra(Constants.NR_ANSWERED_QUESTIONS, currentQuestion);
-				intent.putExtra(Constants.TRANSITION_OBJECT_UUID, questUuid);
-				intent.putExtra(Constants.QUEST_SIZE, QuestionView.this.place.getQuestions().size());
-				intent.putExtra(Constants.CORRECT_ANSWER, question.getCorrectAnswer().get().getAnswer());
-				intent.putExtra(Constants.BOOL_CORRECT_ANSWER, correct);
-				QuestionView.this.timer.cancel();
-				startActivity(intent);
-			}
+            if (selected) {
+                Intent intent = new Intent(QuestionView.this, QuestionResult.class);
+                intent.putExtra(Constants.NR_ANSWERED_QUESTIONS, currentQuestion);
+                intent.putExtra(Constants.TRANSITION_OBJECT_UUID, questUuid);
+                intent.putExtra(Constants.QUEST_SIZE, QuestionView.this.place.getQuestions().size());
+                intent.putExtra(Constants.CORRECT_ANSWER, question.getCorrectAnswer().get().getAnswer());
+                intent.putExtra(Constants.BOOL_CORRECT_ANSWER, correct);
+                QuestionView.this.timer.cancel();
+                startActivity(intent);
+            }
 
-		}
+        }
 
-	}
+    }
 
-	private class NoIdeaClickListener implements OnClickListener {
+    private class NoIdeaClickListener implements OnClickListener {
 
-		private Question question;
+        private Question question;
 
-		public NoIdeaClickListener(Question q) {
-			this.question = q;
-		}
+        public NoIdeaClickListener(Question q) {
+            this.question = q;
+        }
 
-		@Override
-		public void onClick(View v) {
-			Intent intent = new Intent(QuestionView.this, QuestionResult.class);
-			intent.putExtra(Constants.NR_ANSWERED_QUESTIONS, currentQuestion);
-			intent.putExtra(Constants.TRANSITION_OBJECT_UUID, questUuid);
-			intent.putExtra(Constants.QUEST_SIZE, QuestionView.this.place.getQuestions().size());
-			intent.putExtra(Constants.CORRECT_ANSWER, question.getCorrectAnswer().get().getAnswer());
-			intent.putExtra(Constants.BOOL_CORRECT_ANSWER, false);
-			QuestionView.this.timer.cancel();
-			startActivity(intent);
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(QuestionView.this, QuestionResult.class);
+            intent.putExtra(Constants.NR_ANSWERED_QUESTIONS, currentQuestion);
+            intent.putExtra(Constants.TRANSITION_OBJECT_UUID, questUuid);
+            intent.putExtra(Constants.QUEST_SIZE, QuestionView.this.place.getQuestions().size());
+            intent.putExtra(Constants.CORRECT_ANSWER, question.getCorrectAnswer().get().getAnswer());
+            intent.putExtra(Constants.BOOL_CORRECT_ANSWER, false);
+            QuestionView.this.timer.cancel();
+            startActivity(intent);
 
-		}
+        }
 
-	}
+    }
+    
+    private class QuestionTimer extends CountDownTimer {
+        
+        private long[] secPattern = {0, 200, 100};
+        private long[] endPattern = {0, 300, 100, 200, 100};
+        private Vibrator v;
+        private Button counter;
+        private String answer;
+        
+        public QuestionTimer(long time, long thick, Button counter, String answer) {
+            super(time, thick);
+            this.v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            this.counter = counter;
+            this.answer = answer;
+        }
 
-	private class QuestionTimer extends CountDownTimer {
+        @Override
+        public void onFinish() {
+            v.vibrate(endPattern, -1);
+            counter.setText("0");
+            Toast.makeText(QuestionView.this, "Time is up!", Toast.LENGTH_SHORT).show();
 
-		private long[] secPattern = { 0, 200, 100 };
-		private long[] endPattern = { 0, 300, 100, 200, 100 };
-		private Vibrator v;
-		private Button counter;
-		private String answer;
+            Intent intent = new Intent(QuestionView.this, QuestionResult.class);
+            intent.putExtra(Constants.NR_ANSWERED_QUESTIONS, currentQuestion);
+            intent.putExtra(Constants.TRANSITION_OBJECT_UUID, questUuid);
+            intent.putExtra(Constants.QUEST_SIZE, QuestionView.this.place.getQuestions().size());
+            intent.putExtra(Constants.CORRECT_ANSWER, answer);
+            intent.putExtra(Constants.BOOL_CORRECT_ANSWER, false);
+            startActivity(intent);
+            
+        }
 
-		public QuestionTimer(long time, long thick, Button counter, String answer) {
-			super(time, thick);
-			this.v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-			this.counter = counter;
-			this.answer = answer;
-		}
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long sec = (millisUntilFinished / 1000);
+            counter.setTextColor(this.calcColor(sec));
+            counter.setText("" + sec);
+            
+        }
+        
+        private int calcColor(long sec) {
+            int color = Color.GREEN;
 
-		@Override
-		public void onFinish() {
-			v.vibrate(endPattern, -1);
-			counter.setText("0");
-			Toast.makeText(QuestionView.this, "Time is up!", Toast.LENGTH_SHORT).show();
+            if (sec < 15 && sec > 5) {
+                color = Color.YELLOW;
+            } else if (sec <= 5) {
+                color = Color.RED;
+            }
+            
+            
+            if (sec == 3) {
+                v.vibrate(secPattern, -1);
+            } else if (sec == 2) {
+                v.vibrate(secPattern, -1);
+            } else if (sec == 1) {
+                v.vibrate(secPattern, -1);
+            }
 
-			Intent intent = new Intent(QuestionView.this, QuestionResult.class);
-			intent.putExtra(Constants.NR_ANSWERED_QUESTIONS, currentQuestion);
-			intent.putExtra(Constants.TRANSITION_OBJECT_UUID, questUuid);
-			intent.putExtra(Constants.QUEST_SIZE, QuestionView.this.place.getQuestions().size());
-			intent.putExtra(Constants.CORRECT_ANSWER, answer);
-			intent.putExtra(Constants.BOOL_CORRECT_ANSWER, false);
-			startActivity(intent);
+            return color;
+        }
 
-		}
-
-		@Override
-		public void onTick(long millisUntilFinished) {
-			long sec = (millisUntilFinished / 1000);
-			counter.setTextColor(this.calcColor(sec));
-			counter.setText("" + sec);
-
-		}
-
-		private int calcColor(long sec) {
-			int color = Color.GREEN;
-
-			if (sec < 15 && sec > 5) {
-				color = Color.YELLOW;
-			} else if (sec <= 5) {
-				color = Color.RED;
-			}
-
-			if (sec == 3) {
-				v.vibrate(secPattern, -1);
-			} else if (sec == 2) {
-				v.vibrate(secPattern, -1);
-			} else if (sec == 1) {
-				v.vibrate(secPattern, -1);
-			}
-
-			return color;
-		}
-
-	}
+    
+    }
 }
